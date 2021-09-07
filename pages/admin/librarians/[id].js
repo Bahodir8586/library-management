fd7ce832659a417aeb33eb424911c24ade9d767a
Bahodir8586 <bahodira213@gmail.com>
2021-09-07 16:53:34 +0500

    librarian edit page for admin. modals are added

--- pages/admin/librarians/[id].js
+++ pages/admin/librarians/[id].js
@@ -5,13 +5,19 @@ import {useRouter} from "next/router";
 import axios from "../../../utils/axios";
 import EditLibrarian from "../../../components/admin/editLibrarian";
 import Spinner from "../../../components/loaders/spinner/spinner";
+import FailModal from "../../../components/modals/failModal";
+import SuccessModal from "../../../components/modals/successModal";
 
 const Librarian = () => {
 	const router = useRouter()
 	const id = router.asPath.split("/")[3]
 	const [data, setData] = useState({})
+
 	const [isLoading, setIsLoading] = useState(false)
-	const [isError, setIsError] = useState(false)
+	const [successText, setSuccessText]=useState("")
+	const [showSuccessModal,setShowSuccessModal]=useState(false)
+	const [errorText, setErrorText]=useState("")
+	const [showFailModal,setShowFailModal]=useState(false)
 
 	useEffect(() => {
 		setIsLoading(true)
@@ -22,7 +28,6 @@ const Librarian = () => {
 		}).catch((error) => {
 			console.log(error)
 			setIsLoading(false)
-			setIsError(true)
 		})
 	}, [router.asPath])
 
@@ -33,16 +38,35 @@ const Librarian = () => {
 		submitData.append("fullName", fullName);
 		submitData.append("username", username);
 		submitData.append("image", image)
+		if(!username|| !fullName){
+			setShowFailModal(true)
+			setErrorText("Please fill all the fields")
+			return
+		}
+		setIsLoading(true)
 		axios.post(`/admin/librarians/${id}`, submitData).then(response => {
 			console.log(response)
-			router.push("/admin/librarians")
+			setIsLoading(false)
+			setSuccessText("Librarian information successfully edited")
+			setShowSuccessModal(true)
 		}).catch(error => {
 			console.log(error)
+			setIsLoading(false)
+			setErrorText("Something went wrong. Please try again later")
+			setShowFailModal(true)
 		})
 	}
 	return (
 		<Layout>
-			{isLoading ? <Spinner/> : isError ? <div>Error</div> : <EditLibrarian
+			<SuccessModal show={showSuccessModal} title={"Congratulations"} onConfirm={() => {
+				setShowSuccessModal(false)
+				router.push("/admin/librarians");
+			}} text={successText}/>
+			<FailModal show={showFailModal} title={"Error"} onConfirm={() => {
+				setErrorText("")
+				setShowFailModal(false)
+			}} text={errorText}/>
+			{isLoading ? <Spinner/> : <EditLibrarian
 				handleSubmit={(e, username, fullName, image) => handleSubmit(e, username, fullName, image)}
 				data={data}/>}
 
